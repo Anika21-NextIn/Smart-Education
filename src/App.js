bd9acddc7f568b89e154ced12eb32d8ef5616772
Anika21-NextIn <anika21.nextin@gmail.com>
2022-03-11 20:11:54 +0600

    add courses

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home/Home/Home';
+import Contact from './pages/Contact/Contact';
+import Courses from './pages/Courses/Courses';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home></Home> 
+          </Route>
+          <Route exact path="/contact">
+            <Contact></Contact>
+          </Route>
+          <Route exact path="/courses">
+            <Courses></Courses>
           </Route>
         </Switch>
       </Router>
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <>
-            <div id="preloader">
+            {/* <div id="preloader">
                 <div className="loader-container">
                     <div className="progress-br float shadow">
                         <div className="progress__item"></div>
                     </div>
                 </div>
-            </div>
+            </div> */}
             <header className="top-navbar">
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className="container-fluid">
@@ -23,16 +24,18 @@ const Header = () => {
                         </button>
                         <div className="collapse navbar-collapse" id="navbars-host">
                             <ul className="navbar-nav ml-auto">
-                                <li className="nav-item active"><a className="nav-link" href="index.html">Home</a></li>
+                                <li className="nav-item active"><Link className="nav-link" to="/">Home</Link></li>
                                 <li className="nav-item"><a className="nav-link" href="about.html">About Us</a></li>
-                                <li className="nav-item dropdown">
+                                {/* <li className="nav-item dropdown">
                                     <a className="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Course </a>
                                     <div className="dropdown-menu" aria-labelledby="dropdown-a">
                                         <a className="dropdown-item" href="course-grid-2.html">Course Grid 2 </a>
                                         <a className="dropdown-item" href="course-grid-3.html">Course Grid 3 </a>
                                         <a className="dropdown-item" href="course-grid-4.html">Course Grid 4 </a>
                                     </div>
-                                </li>
+                                </li> */}
+                                <li className="nav-item"><Link className="nav-link" to="/courses">Courses</Link></li>
+
                                 <li className="nav-item dropdown">
                                     <a className="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Blog </a>
                                     <div className="dropdown-menu" aria-labelledby="dropdown-a">
@@ -42,7 +45,8 @@ const Header = () => {
                                 </li>
                                 <li className="nav-item"><a className="nav-link" href="teachers.html">Teachers</a></li>
                                 <li className="nav-item"><a className="nav-link" href="pricing.html">Pricing</a></li>
-                                <li className="nav-item"><a className="nav-link" href="contact.html">Contact</a></li>
+                                <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+                                
                             </ul>
                             <ul className="nav navbar-nav navbar-right">
                                 <li><a className="hover-btn-new log orange" href="#" data-toggle="modal" data-target="#login"><span>Book Now</span></a></li>
